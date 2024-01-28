038ad9cb631905ef57e56f5557275164729e8c82
Luis Fernando Cutilak <luiscutilak@gmail.com>
2024-01-28 12:37:02 -0300

    feat: inneritance, Herança

--- src/Poo.kt
+++ src/Poo.kt
@@ -2,6 +2,25 @@
 
 
 // ******************************** (Inheritance) HERANÇA SIMPLES ******************************************
-fun main() {
 
+/*
+open class Dog {      // open class pode ser usada como extensao de outra classe - sem o * Open * nao conseguimos extender essa classe
+    open fun sayHello() {
+        println("wow wow!");
+    }
 }
+
+class Yorkshire : Dog() {  // Si le, yorkshire é um chachorro, ou seja ele herda todas as caracteristicas da classe Dog()
+    override fun sayHello(){  // sobreescrever a função sayHello() herdada da classe Dog()
+        println("wif wif!")
+    }
+}
+
+fun main() {
+    val dog = Yorkshire()
+    dog.sayHello()
+}
+
+ */
+
+//
