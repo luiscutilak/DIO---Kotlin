bb1e72d43ba7bec4cb7a58b556bf2099dd874311
Luis Fernando Cutilak <luiscutilak@gmail.com>
2024-01-28 12:49:24 -0300

    feat: inheritance with parameter, Heranca com parametro

--- src/Poo.kt
+++ src/Poo.kt
@@ -23,4 +23,17 @@ fun main() {
 
  */
 
-//
+// **************************** Herança Com Parametro Construtor *************************************
+
+open class Tiger(val origin: String) {   // a classe Tiger, esta declarada como Open, ou seja, pode extender a uma classe filha, e receber parametros
+    fun sayHello() {                                        // a funcao sayHello() nao extende a outras classes. ´Pois nao tem o Open
+        println("A Tiger from $origin says: grrrrhhhh!")
+    }
+}
+
+class SiberianTiger : Tiger("Siberia") // Criamos a classe filha SiberianTiger : Herdada da classe Tiger que espera um parametro do tipo String
+
+fun main() {
+    val tiger: Tiger = SiberianTiger()          //instanciando OBJETO, SiberianTiger(), Herdado da classe Tiger,
+    tiger.sayHello()   // chamando a função já instanciada com nome tiger. chamando a função sayHello()
+}
