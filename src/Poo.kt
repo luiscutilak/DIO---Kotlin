cc59d76b5f6d8323c6ba1ea6355e390950bdf4bd
Luis Fernando Cutilak <luiscutilak@gmail.com>
2024-02-05 09:05:58 -0300

    feat: herança passando argumentos do construtor para a Superclasse.

--- src/Poo.kt
+++ src/Poo.kt
@@ -25,6 +25,8 @@ fun main() {
 
 // **************************** Herança Com Parametro Construtor *************************************
 
+
+/*
 open class Tiger(val origin: String) {   // a classe Tiger, esta declarada como Open, ou seja, pode extender a uma classe filha, e receber parametros
     fun sayHello() {                                        // a funcao sayHello() nao extende a outras classes. ´Pois nao tem o Open
         println("A Tiger from $origin says: grrrrhhhh!")
@@ -37,3 +39,42 @@ fun main() {
     val tiger: Tiger = SiberianTiger()          //instanciando OBJETO, SiberianTiger(), Herdado da classe Tiger,
     tiger.sayHello()   // chamando a função já instanciada com nome tiger. chamando a função sayHello()
 }
+
+ */
+
+// ***************************** Herança Passando Argumentos do Construtor para a Superclasse ******************************
+
+open class Lion(val name: String, val origin:String) {
+    fun sayHello(){
+        print ("$name, the lion from $origin says: graoh!")
+    }
+
+}
+
+class Asiatic(name: String) : Lion(name = name, origin = "India")
+fun main() {
+    val lion: Lion = Asiatic("Rufo")
+    lion.sayHello()
+}
+
+// exploramos os 4 conceitos da Orientação a objetos.
+
+// * Abstração
+// * Encapsulamento ( todas as funções são finally, não tem como sobrescrever)
+// * Herança
+// * Polimorfismo - a questão que podemos tratar o "Lion" de uma forma mais genérica! ou o "Asiatic" como uma forma mais especifica.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
